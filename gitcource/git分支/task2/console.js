065a597f41b556d854702fc3b25f417aff91107f
sam.yang <sam.yang@itnsl.net>
2020-09-24 18:00:47 +0800

    add feature2

--- gitcource/git分支/task2/console.js
+++ gitcource/git分支/task2/console.js
@@ -4,3 +4,12 @@ for(x=3;x<=15;x++) {
     console.log(`${x}x${y}`);
   }
 }
+
+function input(cm,kg){
+  const m = cm/100;
+  const bmi = (kg/Math.pow(m,2)).toFixed(1);
+  console.log(bmi);
+  
+}
+
+input(178,70);
